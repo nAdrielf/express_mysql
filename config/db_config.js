4422b4371950188a12924480a4610fd33c2e6dfd
Naufal Adriel Fauzi <fnaufaladriel@gmail.com>
2021-12-31 20:51:09 +0700

    database connected

---
+++ config/db_config.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql');
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'race_teamsdb',
+});
+
+conn.connect(function (err) {
+  if (err) throw err;
+  console.log('database connected!!');
+});
