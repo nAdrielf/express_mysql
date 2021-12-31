3671f4daf7fd6cdcb0a66abacf475b4be15e6e26
Naufal Adriel Fauzi <fnaufaladriel@gmail.com>
2021-12-31 23:21:29 +0700

    delete data berhasil

--- routes/controller.js
+++ routes/controller.js
@@ -36,11 +36,22 @@ router.put('/:id', (req, res) => {
   let name = req.body.name;
   let phone = req.body.phone;
   let email = req.body.email;
-  let sql = 'UPDATE team_principles SET name=?,phone=?,email=? WHERE id=?'; //gak bisa salah satu yg diisi, kudu diisi seua valuenya
+  let sql = 'UPDATE team_principles SET name=?,phone=?,email=? WHERE id=?'; //gak bisa salah satu yg diisi/diganti, kudu diisi seua valuenya jgn lupa isi idnya juga
   db.query(sql, [name, phone, email, id], function (err, result) {
     if (err) throw err;
     res.json({
       msg: `team principal by id of ${req.params.id} has been changed`,
+      result,
+    });
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  let sql = 'DELETE FROM team_principles WHERE id=?';
+  db.query(sql, req.params.id, function (err, result) {
+    if (err) throw err;
+    res.json({
+      msg: `racing team by id of ${req.params.id} has been deleted`,
       result,
     });
   });
